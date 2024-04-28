3f3f6708c540af24852785b280273a5eb6cb6b1d
legonma <marianolegon@gmail.com>
2024-04-28 12:48:17 -0300

    Create PrimaryButton

--- Front (Mobile)/resumen_mobile/lib/presentation/screen/login_screen.dart
+++ Front (Mobile)/resumen_mobile/lib/presentation/screen/login_screen.dart
@@ -3,19 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:resumen_mobile/presentation/screen/home_screen.dart';
 import '../uicoreStyles/uicore_input_style.dart';
+import '../uicoreStyles/uicore_primary_button_style.dart';
 import '../uicoreStyles/uicore_title_style.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class LoginScreen extends StatelessWidget {
   static const String name = 'LoginScreen';
-  
   //Aca creo que iria un atributo para guardar lo del form o input.
-
-  LoginScreen({super.key});
-
   final TextEditingController _inputUsernameController = TextEditingController();
   final TextEditingController _inputPassController = TextEditingController();
+
+  LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -49,23 +48,10 @@ class LoginScreen extends StatelessWidget {
                //Agrego espacio al boton
               const SizedBox(height: 10),
               //aca va el login button
-              ElevatedButton(
-                onPressed: () {
-                  print(_inputUsernameController.text);
-                  print(_inputPassController.text);
-                  context.pushNamed(HomeScreen.name);
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF243035)),
-                  elevation: MaterialStateProperty.all<double>(20), // Ajusta la elevación para la sombra exterior
-                  overlayColor: MaterialStateProperty.all<Color>(const Color.fromARGB(0, 3, 3, 3)), // Elimina el color de superposición para un efecto más suave
-                  shadowColor: MaterialStateProperty.all<Color>(const Color.fromARGB(177, 3, 3, 3).withOpacity(0.4)), // Color de la sombra
-                  
-                ),
-                child: const TitleStyle(
-                  text: 'Login',
-                ),
-              )
+              PrimaryButton(
+                caller: onClickButtonLogin(context),
+                text: 'Login',
+              ),
             ],
           ),
         ),
@@ -73,34 +59,40 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-Future<bool> sendLoginData(String username, String password) async {
-  bool loginOk = false;
-  final url = Uri.parse('http://localhost:8080/api/login'); // Reemplaza con la URL de tu servidor Node.js
-  final response = await http.post(
-    url,
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      'username': username,
-      'password': password,
-    }),
-  );
-  //CREEMOS QUE EL STATUSCODE SIEMPRE ES 200 OK
-  if (response.statusCode == 200) {
-    // Si la solicitud es exitosa, imprime la respuesta del servidor
-    print('Respuesta del servidor: ${response.body}');
+  onClickButtonLogin(context) {
+    print(_inputUsernameController.text);
+    print(_inputPassController.text);
+    context.goNamed(HomeScreen.name);
+  }
 
-    loginOk = true;
-    
-    // Aquí puedes manejar la respuesta del servidor como desees
-    // Por ejemplo, puedes convertir la respuesta JSON en un objeto Dart
-    //final Map<String, dynamic> userData = jsonDecode(response.body);
-    // Y usar los datos del usuario en tu aplicación
-  } //else {
-    // Si la solicitud no es exitosa, imprime el mensaje de error
-    //print('Error al enviar los datos de inicio de sesión: ${response.statusCode}');
-    //}
-    return loginOk; 
-}
+  Future<bool> sendLoginData(String username, String password) async {
+    bool loginOk = false;
+    final url = Uri.parse('http://localhost:8080/api/login'); // Reemplaza con la URL de tu servidor Node.js
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': username,
+        'password': password,
+      }),
+    );
+    //CREEMOS QUE EL STATUSCODE SIEMPRE ES 200 OK
+    if (response.statusCode == 200) {
+      // Si la solicitud es exitosa, imprime la respuesta del servidor
+      print('Respuesta del servidor: ${response.body}');
+
+      loginOk = true;
+      
+      // Aquí puedes manejar la respuesta del servidor como desees
+      // Por ejemplo, puedes convertir la respuesta JSON en un objeto Dart
+      //final Map<String, dynamic> userData = jsonDecode(response.body);
+      // Y usar los datos del usuario en tu aplicación
+    } //else {
+      // Si la solicitud no es exitosa, imprime el mensaje de error
+      //print('Error al enviar los datos de inicio de sesión: ${response.statusCode}');
+      //}
+      return loginOk; 
+  }
 }

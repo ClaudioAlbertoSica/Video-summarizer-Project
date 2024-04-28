5e81891378ee416f79ab55bdd5a963cbc7709f15
ClaudioAlbertoSica <ClaudioAlbertoSica@gmail.com>
2024-04-28 14:17:13 -0300

    Added: cors (allows to work with the front end)

--- Back/resumenAIbackend/server.js
+++ Back/resumenAIbackend/server.js
@@ -2,9 +2,10 @@ import express from 'express'
 import RouterUsuario from './router/users.js'
 //import RouterRes from './router/resumen.js'
 import CnxMongoDB from './models/connection/mongodb.js'
+import cors from 'cors'
 
 
-class Server{
+class Server {
     constructor(port, persistencia) {
         this.port = port
         this.persistencia = persistencia
@@ -13,8 +14,9 @@ class Server{
     }
 
     async start() {
+        this.app.use(cors())
         this.app.use(express.json())
-        this.app.use(express.urlencoded({extended: true}))
+        this.app.use(express.urlencoded({ extended: true }))
         this.app.use(express.static('public'))
         this.app.use('/api', new RouterUsuario(this.persistencia).start())
         if (this.persistencia == 'MONGODB') {
@@ -22,8 +24,8 @@ class Server{
         }
         const PORT = this.port
 
-        this.server = this.app.listen(PORT, () => {console.log(`SERVIDOR EXPRESS ESCUCHANDO EN http://localhost:${PORT}`)})
-        this.server.on('Error', (error) => {console.log(`ERROR EN SERVIDOR: ${error.message}`)} )
+        this.server = this.app.listen(PORT, () => { console.log(`SERVIDOR EXPRESS ESCUCHANDO EN http://localhost:${PORT}`) })
+        this.server.on('Error', (error) => { console.log(`ERROR EN SERVIDOR: ${error.message}`) })
         return this.app
 
     }

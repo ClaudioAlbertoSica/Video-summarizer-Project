229f9e0aa6f1d8dedf6d059064aafa00c291dcd4
rociobani <134657672+rociobani@users.noreply.github.com>
2024-05-05 14:16:14 -0300

    Creación de FormVideo

--- Front (Mobile)/resumen_mobile/lib/presentation/screen/form_video_screen.dart
+++ Front (Mobile)/resumen_mobile/lib/presentation/screen/form_video_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:resumen_mobile/presentation/uicoreStyles/uicore_title_style.dart';
 
 import '../uicoreStyles/uicore_app_title_style.dart';
+
+enum Idiomas{ english, spanish}
 
 class CoreFormVideo extends StatelessWidget {
   const CoreFormVideo({super.key});
@@ -48,7 +51,7 @@ class _FormVideoState extends State<FormVideo> {
   bool shortValue = false;
   bool transcrpit = false;
   bool imagesObtain = false;
-  
+  Idiomas idiomaSeleccionado = Idiomas.spanish; 
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -89,6 +92,50 @@ class _FormVideoState extends State<FormVideo> {
               setState((){});
             },
           ),
+          ExpansionTile(
+            title: const Text('Idioma'),
+            subtitle: const Text('Selecciona el idioma.'),
+            children:[
+              RadioListTile(
+                title: Text(Idiomas.english.name),
+                value: Idiomas.english, 
+                groupValue: idiomaSeleccionado, 
+                onChanged: (value){
+                  idiomaSeleccionado = value as Idiomas;
+                  setState(() {});
+                }
+                ),
+              RadioListTile(
+                title: Text(Idiomas.spanish.name),
+                value: Idiomas.spanish, 
+                groupValue: idiomaSeleccionado, 
+                onChanged: (value){
+                  idiomaSeleccionado = value as Idiomas;
+                  setState(() {});
+                }
+                )
+            ]),
+            TextFormField(
+                  decoration: const InputDecoration(
+                  hintText: 'Titulo (Opcional)',
+                  border: OutlineInputBorder(
+                    borderRadius:  BorderRadius.all(Radius.circular(10)),
+                    )
+                  ),
+                ),
+          ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF243035)),
+                  elevation: MaterialStateProperty.all<double>(20), // Ajusta la elevación para la sombra exterior
+                  overlayColor: MaterialStateProperty.all<Color>(const Color.fromARGB(0, 3, 3, 3)), // Elimina el color de superposición para un efecto más suave
+                  shadowColor: MaterialStateProperty.all<Color>(const Color.fromARGB(177, 3, 3, 3).withOpacity(0.4)), // Color de la sombra
+                  
+                ),
+                child: const TitleStyle(
+                  text: 'Crear Resumen',
+                ),
+              ),
         ],
       ),
     );

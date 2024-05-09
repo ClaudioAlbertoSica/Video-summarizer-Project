8e9727c055be09df92369f2875d0f1a904240676
rociobani <134657672+rociobani@users.noreply.github.com>
2024-05-08 23:37:05 -0300

    DarkMode dinámico con Riverpod Solucionado

--- Front (Mobile)/resumen_mobile/lib/main.dart
+++ Front (Mobile)/resumen_mobile/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:resumen_mobile/core/app_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:resumen_mobile/entity/app_theme.dart';
+import 'package:resumen_mobile/presentation/providers/theme_provider.dart';
 
 final userProvider = StateProvider<String?>((ref) => null);
 
@@ -12,12 +12,13 @@ void main() {
   ));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   MainApp({super.key});
-  final theme = AppTheme(selectedColor: 0, isDark: false);
+  
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final theme = ref.watch(themeNotifierProvider);
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,

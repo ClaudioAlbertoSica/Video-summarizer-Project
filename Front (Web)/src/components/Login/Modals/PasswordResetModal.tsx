4a67f19059ee70534140e4d57c8edbb443a7a4cf
ClaudioAlbertoSica <ClaudioAlbertoSica@gmail.com>
2024-05-25 00:03:10 -0300

    Password Reset Modal, now works with the server.

--- Front (Web)/src/components/Login/Modals/PasswordResetModal.tsx
+++ Front (Web)/src/components/Login/Modals/PasswordResetModal.tsx
@@ -1,27 +1,60 @@
-import { TextField, Box, Button, Container, Link, Typography } from "@mui/material";
+import { TextField, Box, Button, Container, Link, Typography, Alert } from "@mui/material";
 import "./Modals.css";
 import { ModalNames } from "./ImTheActiveModal.ts";
+import { FormEvent, useRef, useState } from "react";
+import server from "../../../Services/serverCall.ts";
+import { AlertMessage, alertMessagesHandler, alertTypes } from "../../../Services/alertMessagesHandler.ts";
 
 interface LoginModalSelector {
   selectorCallback: (modalName: ModalNames) => void;
 }
 
 function PasswordResetModal({ selectorCallback }: LoginModalSelector) {
+  const [alertToShow, setAlertToShow] = useState<AlertMessage>({ message: "don't show", type: alertTypes.info });
+  const formRef = useRef<HTMLFormElement>();
+  const defaultAlertMessage: string = "Hubo un problema...";
+  const notFountUSer: string = "Usuario no registrado, por favor: cree uno nuevo";
+  const confirmationMessage: string = "Contraseña recuperada: verifique su eMail";
+  const requestSent: string = "Consultando los datos de su cuenta";
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    const formData = new FormData(formRef?.current);
+    const submittedEmail: string = formData?.get("yourEmail") as string;
+    alertMessagesHandler(setAlertToShow, requestSent, alertTypes.info);
+    await server
+      .post<boolean>("/recuperar", { userName: submittedEmail })
+      .then((res) => {
+        if (res.data) {
+          alertMessagesHandler(setAlertToShow, confirmationMessage, alertTypes.success, 3000);
+        } else {
+          alertMessagesHandler(setAlertToShow, notFountUSer, alertTypes.info, 3000);
+        }
+        setTimeout(() => selectorCallback(ModalNames.Login), 3000);
+      })
+      .catch((err) => {
+        alertMessagesHandler(setAlertToShow, err.error || defaultAlertMessage, alertTypes.error);
+      });
+  };
+
   return (
     <Container className="ExternalLoginContainer">
-      <Box className="FormBox" component="form">
+      <Box className="FormBox" component="form" ref={formRef} onSubmit={handleSubmit}>
         <Typography variant="h3" textAlign="center" gutterBottom>
           ¿Olvidó su contraseña?
         </Typography>
         <Typography variant="subtitle1" textAlign="center" gutterBottom>
           Ingrese su email. Le será enviado un correo con una nueva contraseña provisoria, deberá cambiar al ingresar.
         </Typography>
+        <Container className="AlertsContainer">
+          {alertToShow.message !== "don't show" && <Alert severity={alertToShow.type}> {alertToShow.message} </Alert>}
+        </Container>
         <TextField
           className="FormInputs"
           id="outlined-search"
           name="yourEmail"
           label="Ingrese su e-mail"
-          type="password"
+          type="email"
           variant="outlined"
           required
         />

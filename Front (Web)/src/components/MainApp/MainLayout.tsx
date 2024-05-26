ab2814aeb4c1eb7ef9ff5df5e0451a922353d57c
ClaudioAlbertoSica <ClaudioAlbertoSica@gmail.com>
2024-05-25 23:29:43 -0300

    Removed a semi-colon from the MainLayout

--- Front (Web)/src/components/MainApp/MainLayout.tsx
+++ Front (Web)/src/components/MainApp/MainLayout.tsx
@@ -69,7 +69,7 @@ function GridLayout({ isProvisoryPassword }: InitialInfo) {
     <SelectedSummaryContext.Provider value={{ State: selectedSummary, SetState: setSelectedSummary }}>
       <ButtonViewContext.Provider value={setSelectedCentralPanelView}>
         <Container className="ExternalContainer">
-          <Container className="SidePanelContainer" sx={{ backgroundColor: myTheme.palette.my.header}}>
+          <Container className="SidePanelContainer" sx={{ backgroundColor: myTheme.palette.my.header }}>
             <SidePanel />
           </Container>
 
@@ -93,8 +93,8 @@ function GridLayout({ isProvisoryPassword }: InitialInfo) {
           The code makes use of the ImTheActiveView() function, that will only be true for the desired View.
           Close Button below shoots "noneSelected", so no view (and no button) is displayed at all.
           */}
-            <Grid className="CentralPanel" item xs={12} sx={{ backgroundColor: myTheme.palette.my.sidePanelBg}}>
-              {checkForForcedView()};
+            <Grid className="CentralPanel" item xs={12} sx={{ backgroundColor: myTheme.palette.my.sidePanelBg }}>
+              {checkForForcedView()}
             </Grid>
             <Grid className="FooterGrid" item xs={12}>
               <h1>Footer</h1>

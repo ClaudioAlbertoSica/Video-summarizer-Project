a8e80e80af6f8b1f4577a53b34242439e0756f44
Claudio Alberto Sica <claudioalbertosica@gmail.com>
2024-06-07 18:53:24 -0300

    Added a Dialog to the bottom Delete Button, asking for confirmation before permanentely deleteing a document.

--- Front (Web)/src/components/MainApp/Views/Buttons/DeletePDF.tsx
+++ Front (Web)/src/components/MainApp/Views/Buttons/DeletePDF.tsx
@@ -1,22 +1,50 @@
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Button } from "@mui/material";
+import { Alert, Button, Container } from "@mui/material";
 import { useContext } from "react";
-import { LoggedUserContext } from "../../../../ActiveUserContext";
-import { SelectedSummaryContext, defaultSummary } from "../../SelectedSummaryContext";
-import server from "../../../../../src/Services/serverCall.ts";
+import { LoggedUserContext } from "../../../../ActiveUserContext.ts";
+import { SelectedSummaryContext, defaultSummary } from "../../SelectedSummaryContext.ts";
+import server from "../../../../Services/serverCall.ts";
 import { LoggedUser, Summary } from "../../../../Services/Types/UserTypes.ts";
 import { ButtonViewContext } from "../../ButtonViewContext.ts";
 import { ValidViewNames } from "../ImTheActiveView.ts";
+import { AlertMessage, alertMessagesHandler, alertTypes } from "../../../../Services/alertMessagesHandler.ts";
 
-function DeletePDFbtn() {
+export default function DeletePDF() {
+  const [open, setOpen] = useState(false);
   const loggedUser = useContext(LoggedUserContext);
   const summaryContext = useContext(SelectedSummaryContext);
   const setSelectedCentralPanelView = useContext(ButtonViewContext);
+  const [alertToShow, setAlertToShow] = useState<AlertMessage>({ message: "don't show", type: alertTypes.info });
+  const serverRequestedMessage: string = "Solicitud enviada al servidor";
+  const confirmationMessage: string = "Documento borrado";
+  const defaultAlertMessage: string = "Problema en el servidor";
 
-  const handleClick = async () => {
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleReject = () => {
+    setOpen(false);
+  };
+
+  const handleAccept = async () => {
+    await serverRequest();
+    setTimeout(() => {
+      setOpen(false);
+      setSelectedCentralPanelView(ValidViewNames.noneSelected);
+    }, 800);
+  };
+
+  const serverRequest = async () => {
     const idresSelected = summaryContext.State.idres;
     const selectedResDBindex = loggedUser.userState.inventario.findIndex((sum) => sum.idres === idresSelected);
-
+    alertMessagesHandler(setAlertToShow, serverRequestedMessage, alertTypes.info);
     await server
       .delete<Summary>(`/${loggedUser.userState.id}/resumen/${summaryContext.State.idres}`)
       .then(() => {
@@ -24,27 +52,49 @@ function DeletePDFbtn() {
         newUserContext.inventario.splice(selectedResDBindex, 1);
         loggedUser.userSteState(newUserContext);
         summaryContext.SetState(defaultSummary);
-        setSelectedCentralPanelView(ValidViewNames.noneSelected);
+        alertMessagesHandler(setAlertToShow, confirmationMessage, alertTypes.success);
       })
       .catch((err) => {
         console.log(err.error);
         console.log("Error on update");
+        alertMessagesHandler(setAlertToShow, err?.error || defaultAlertMessage, alertTypes.error);
       });
   };
 
   return (
-    <Button
-      className="DeleteButton"
-      variant="outlined"
-      color="error"
-      startIcon={<DeleteIcon />}
-      onClick={() => {
-        handleClick();
-      }}
-    >
-      Borrar
-    </Button>
+    <>
+      <Button
+        className="DeleteButton"
+        variant="outlined"
+        color="error"
+        startIcon={<DeleteIcon />}
+        onClick={() => {
+          handleClickOpen();
+        }}
+      >
+        Borrar
+      </Button>
+      <Dialog className="DeleteDialogBox" open={open} onClose={handleReject}>
+        <DialogTitle className="DeleteDialogueTitle">¿Está seguro de borrar este resumen?</DialogTitle>
+        <DialogContent>
+          <DialogContentText className="DeleteDialogText">
+            La deleción de un documento es PERMANENTE. Si llegó a esta ventana por error, haga click en Cancelar.
+            <br />
+            Si realmente desea borrar el documento: haga click en Aceptar.
+          </DialogContentText>
+        </DialogContent>
+        <Container className="AlertsContainer">
+          {alertToShow.message !== "don't show" && <Alert severity={alertToShow.type}> {alertToShow.message} </Alert>}
+        </Container>
+        <DialogActions>
+          <Button variant="contained" onClick={handleReject}>
+            Cancelar
+          </Button>
+          <Button onClick={handleAccept} autoFocus>
+            Aceptar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
-
-export default DeletePDFbtn;
